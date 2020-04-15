9a32f578c4b2479770d51921c58acf9978d72436
Isachard Rodriguez <isachard.rodriguez@pcc.edu>
2020-04-14 22:00:50 -0700

    Add files via upload

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+
+
+#encoding categorical data using dummy encoding
+#labelencoder_x = LabelEncoder()
+#x[:,0] = labelencoder_x.fit_transform(x[:,0])
+#onehotenco = OneHotEncoder()
+#x = onehotenco.fit_transform(x).toarray()
+
+ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
+x = ct.fit_transform(x)
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0 )
+
+
+
+
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+
+
+
